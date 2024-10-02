6581b807abca04ced15cc74e0dda5c2991f9add3
Jonathan Yau <jonathan-jcy@hotmail.com>
2024-10-02 15:44:40 -0400

    Added commanded line argument to include file path when executing the script

--- file-organizer.py
+++ file-organizer.py
@@ -1,16 +1,30 @@
 import os
 import shutil
+import argparse
 
-target_folder = '../../Downloads'
-extensions = {item.split('.')[-1] for item in os.listdir(target_folder) if os.path.isfile(os.path.join(target_folder, item))}
+def organize_files(target_folder):
+    # Finds the extensions of the files 
+    extensions = {item.split('.')[-1] for item in os.listdir(target_folder) if os.path.isfile(os.path.join(target_folder, item))}
 
-# Create folder based on extension type
-for extension in extensions:
-    if not os.path.exists(os.path.join(target_folder, extension)):
-        os.mkdir(os.path.join(target_folder, extension))
+    # Create folder based on extension type
+    for extension in extensions:
+        if not os.path.exists(os.path.join(target_folder, extension)):
+            os.mkdir(os.path.join(target_folder, extension))
 
-# move files to target folder
-for item in os.listdir(target_folder):
-    if os.path.isfile(os.path.join(target_folder, item)):
-        file_extension = item.split('.')[-1]
-        shutil.move(os.path.join(target_folder, item), os.path.join(target_folder, file_extension, item ))
+    # move files to target folder
+    for item in os.listdir(target_folder):
+        if os.path.isfile(os.path.join(target_folder, item)):
+            file_extension = item.split('.')[-1]
+            shutil.move(os.path.join(target_folder, item), os.path.join(target_folder, file_extension, item ))
+
+def main(): 
+    #Sets up the command line parsing change parameter in expanduser for default location
+    parser = argparse.ArgumentParser(description= 'Organizes the files based on extension of file')
+    parser.add_argument('path', type=str, nargs='?', default=os.path.expanduser('../../Downloads'), help='Default path ~/Downloads' )
+
+    args = parser.parse_args()
+
+    organize_files(args.path)
+
+if __name__ == "__main__":
+    main()
